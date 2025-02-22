b66481610466350ec4c00d2326ba33c94ddca103
Andrewvlou <andreasvloutis13@gmail.com>
2025-02-22 14:05:55 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,11 @@
+#coding= UTF-8
+import streamlit as st
+st.set_page_config(page_title="Python course",page_icon=":tada:",layout="wide")
+
+#HEADER SECTION
+
+st.subheader("Learning python")
+st.title("Python course")
+st.write("This is a website that redirects you to a full 12 hour FREE course all you have to do is click the link below ")
+st.write("[Start Journey](https://youtu.be/dQw4w9WgXcQ?si=y1_GO-bvzMyj3ksj)")
+
