18cd4aa2b24d7410c531d40ad639fbf9be6888e0
Andrewvlou <andreasvloutis13@gmail.com>
2025-02-23 03:55:13 +0200

    Add files via upload

---
+++ Troll website.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(page_title="Python course",page_icon=":tada:",layout="wide")
+
+#HEADER SECTION
+
+st.subheader("Metallica Tour")
+st.title("Metallica Tour in Athens(OAKA)")
+st.write("Η αγορά εισητηρίων είνια μόνο ενα κλίκ μακρία!!! ")
+st.write("[ΑΓΟΡΑΣΕ ΤΩΡΑ](https://youtu.be/uHgt8giw1LY?si=oPt0X8XiZYntfPPe)")
