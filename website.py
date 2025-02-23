93b551b7ef03166ffe8748f88153b224c060f7b4
Andrewvlou <andreasvloutis13@gmail.com>
2025-02-23 14:22:37 +0200

    Update website.py

--- website.py
+++ website.py
@@ -1,5 +1,4 @@
 #coding= UTF-8
-from tkinter import PhotoImage
 
 import streamlit as st
 st.set_page_config(page_title="Python course",page_icon=":tada:",layout="wide")
@@ -10,4 +9,3 @@ st.subheader("Metallica Tour")
 st.title("Metallica Tour in Athens(OAKA)")
 st.write("Η αγορά εισητηρίων είνια μόνο ενα κλίκ μακρία!!! ")
 st.write("[ΑΓΟΡΑΣΕ ΤΩΡΑ](https://youtu.be/uHgt8giw1LY?si=oPt0X8XiZYntfPPe)")
-my_image= PhotoImage(file="C:/Users/user/OneDrive - International Hellenic University/Pictures")
