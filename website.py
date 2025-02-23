fce9bc7ec4e8959fc934a434a0198b8f4b06b509
Andrewvlou <andreasvloutis13@gmail.com>
2025-02-23 14:16:22 +0200

    Add files via upload

---
+++ website.py
@@ -0,0 +1,13 @@
+#coding= UTF-8
+from tkinter import PhotoImage
+
+import streamlit as st
+st.set_page_config(page_title="Python course",page_icon=":tada:",layout="wide")
+
+#HEADER SECTION
+
+st.subheader("Metallica Tour")
+st.title("Metallica Tour in Athens(OAKA)")
+st.write("Η αγορά εισητηρίων είνια μόνο ενα κλίκ μακρία!!! ")
+st.write("[ΑΓΟΡΑΣΕ ΤΩΡΑ](https://youtu.be/uHgt8giw1LY?si=oPt0X8XiZYntfPPe)")
+my_image= PhotoImage(file="C:/Users/user/OneDrive - International Hellenic University/Pictures")
